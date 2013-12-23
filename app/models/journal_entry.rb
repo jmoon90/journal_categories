6a1d72ce8911ac933c9e2d05e7ef206cbab807bd
John Moon <johnmoonyy@gmail.com>
2013-12-23 14:54:43 -0500

    Fix JournalEntry associations and add inverse relationships

--- app/models/journal_entry.rb
+++ app/models/journal_entry.rb
@@ -1,7 +1,9 @@
 class JournalEntry < ActiveRecord::Base
   validates_presence_of :title
   validates_presence_of :description
-  has_many :journal_categories
-  #has_many :categories,
-  #  through: :journal_categories
+  has_many :journal_categories,
+    inverse_of: :journal_entry
+  has_many :categories,
+    through: :journal_categories,
+    inverse_of: :journal_entries
 end
