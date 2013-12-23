5c894f9b79b7ea85284c58a6635803e982e6bc46
John Moon <johnmoonyy@gmail.com>
2013-12-23 15:36:15 -0500

    Made form for submitting a new journal entry. Need to add category selection and category group display

--- app/controllers/journal_entries_controller.rb
+++ app/controllers/journal_entries_controller.rb
@@ -6,4 +6,19 @@ class JournalEntriesController < ApplicationController
   def new
     @journal_entry = JournalEntry.new
   end
+
+  def create
+    @journal_entry = JournalEntry.new(journal_params)
+    if @journal_entry.save
+      redirect_to journal_entries_path
+    else
+    binding.pry
+      render :new
+    end
+  end
+
+  private
+  def journal_params
+    params.require(:journal_entry).permit(:title, :description)
+  end
 end
