b162284ae33285a8ce06caac28097697e77f3b37
John Moon <johnmoonyy@gmail.com>
2013-12-25 09:08:24 -0500

    Add delete button for journal entries

--- app/controllers/journal_entries_controller.rb
+++ app/controllers/journal_entries_controller.rb
@@ -1,6 +1,7 @@
 class JournalEntriesController < ApplicationController
   def index
     @journal_entries = JournalEntry.all
+    @categories = Category.all
   end
 
   def new
@@ -10,11 +11,20 @@ class JournalEntriesController < ApplicationController
   def create
     @journal_entry = JournalEntry.new(journal_params)
     if @journal_entry.save
+    binding.pry
       flash[:success] = "Success"
       redirect_to journal_entries_path
     else
       flash[:error] = "Error"
       render :new
+    end
+  end
+
+  def destroy
+    @journal_entry = JournalEntry.find(params[:id])
+    if @journal_entry.destroy
+      flash[:success] = "Deleted successfully"
+      redirect_to journal_entries_path
     end
   end
 
--- app/models/journal_entry.rb
+++ app/models/journal_entry.rb
@@ -1,9 +1,9 @@
 class JournalEntry < ActiveRecord::Base
-  validates_presence_of :title
-  validates_presence_of :description
   has_many :journal_categories,
     inverse_of: :journal_entry
   has_many :categories,
     through: :journal_categories,
     inverse_of: :journal_entries
+  validates_presence_of :title
+  validates_presence_of :description
 end
